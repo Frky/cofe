713f00c29d80815ba3abd45ee0d2f18e43b1ceaf
Franck De Goër <degoerdf@mint>
2015-04-14 14:11:58 +0200

    Add hiden features

--- cofe.py
+++ cofe.py
@@ -5,19 +5,33 @@ import argparse
 import itertools
 import time
 import sched
-from random import randint
+from random import randint, choice, seed
 from threading import Thread, Timer
 
 import irc
 import irc.client, irc.bot
 import jaraco.logging
 
-WAIT_BEFORE_QUIT = 3600
-WAIT_BEFORE_JOIN = 3*86400
-WAIT_BEFORE_POST = 18000
-NB_MSG_BEFORE_QUIT = 7
+TESTING = False
 
+if TESTING:
+    WAIT_BEFORE_QUIT = 30
+    WAIT_BEFORE_JOIN = 60
+    WAIT_BEFORE_POST = 30
+    NB_MSG_BEFORE_QUIT = 7
+else:
+    WAIT_BEFORE_QUIT = 3600
+    WAIT_BEFORE_JOIN = 3*86400
+    WAIT_BEFORE_POST = 18000
+    NB_MSG_BEFORE_QUIT = 7
+
+original_nicks = ["Oxdeca", "_0xdec", "Oxcafe", "oxdeca", "oxcafe", "_0xcaf"]
 nicknames = ["Oxdeca", "_0xdeca", "Oxcafe", "oxdeca", "oxcafe"]
+s_nouns = ["A dude", "My mom", "The king", "Some guy", "A cat with rabies", "A sloth", "Your homie", "This cool guy my gardener met yesterday", "Superman"]
+p_nouns = ["These dudes", "Both of my moms", "All the kings of the world", "Some guys", "All of a cattery's cats", "The multitude of sloths living under your bed", "Your homies", "Like, these, like, all these people", "Supermen"]
+s_verbs = ["eats", "kicks", "gives", "treats", "meets with", "creates", "hacks", "configures", "spies on", "retards", "meows on", "flees from", "tries to automate", "explodes"]
+p_verbs = ["eat", "kick", "give", "treat", "meet with", "create", "hack", "configure", "spy on", "retard", "meow on", "flee from", "try to automate", "explode"]
+infinitives = ["to make a pie.", "for no apparent reason.", "because the sky is green.", "for a disease.", "to be able to make toast explode.", "to know more about archeology."]
 
 class Bot(irc.bot.SingleServerIRCBot):
 
@@ -33,6 +47,24 @@ class Bot(irc.bot.SingleServerIRCBot):
 
     def on_welcome(self, c, e):
         c.join(self.target)
+
+    def on_privmsg(self, c, e):
+        global original_nicks
+        auth = e.source
+        msg = e.arguments[0].split(":", 1)[0]
+	print "[{0}] {1}".format(auth, msg)
+        if auth[:min(6, len(auth))] in original_nicks:
+            if msg.count("!to_") > 0:
+                dest = ""
+                idx = msg.index("!to_") + 4
+                while len(msg) > idx and msg[idx] != " ":
+                    dest += msg[idx]
+                    idx += 1
+                c.privmsg(dest, msg[idx + 1:])
+            else:
+                c.privmsg(self.target, msg)
+        else:
+            c.privmsg("Oxcafe_", "[" + auth + "] " + msg)
 
     def on_join(self, c, e):
         self.main_loop(c)
@@ -50,16 +82,20 @@ class Bot(irc.bot.SingleServerIRCBot):
             self.dcc_connect(address, port)
 
     def spam(self, c):
-        c.privmsg(self.target, "(nespresso)") 
+        return
+        c.privmsg(self.target, self.sing_sen_maker()) 
 
     def quit(self, c):
         c.quit("What else ?")
 
     def main_loop(self, connection):
         ttl = randint(1, NB_MSG_BEFORE_QUIT)
+        print "[{0}] TTL: {1}".format(self.nick, ttl)
         next_msg = 0
         for i in xrange(ttl):
-            next_msg += randint(1, WAIT_BEFORE_POST)
+            rdm = randint(1, WAIT_BEFORE_POST)
+            next_msg += rdm 
+	    print "[{0}] Next message in {1} sec".format(self.nick, rdm)
             Timer(next_msg, self.spam, args=[connection]).start()
         Timer(next_msg + WAIT_BEFORE_QUIT, self.quit, args=[connection]).start()
 
@@ -68,17 +104,27 @@ class Bot(irc.bot.SingleServerIRCBot):
         time.sleep(randint(1, WAIT_BEFORE_QUIT))
         nicknames.append(self.nick)
 
+    def sing_sen_maker(self):
+        global s_nouns, s_verbs, infinitives, p_nouns
+        return choice(s_nouns) + " " + choice(s_verbs) + " " + (choice(s_nouns).lower() or choice(p_nouns).lower()) + " " + choice(infinitives)
+
     
 def main():
     global nicknames
+    seed()
     server = "securimag.org"
     port = 6697
-    target = "#securimag"
+    if TESTING:
+        target = "#troll"
+    else:
+        target = "#securimag"
     while True:
         if len(nicknames) > 0:
             bot = Bot(nicknames.pop(), server, port, target)
-            Timer(randint(1, WAIT_BEFORE_JOIN), bot.start).start()
-            print "Bot created"
+            rdm = randint(1, WAIT_BEFORE_JOIN)
+            Timer(rdm, bot.start).start()
+            print "[{0}] created".format(bot.nick)
+            print "[{0}] will join in {1} sec".format(bot.nick, rdm)
         time.sleep(1)
 
 if __name__ == '__main__':
